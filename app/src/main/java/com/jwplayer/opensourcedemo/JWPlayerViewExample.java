9862bb15ecc446ce4432e555ac8e4a1dce1c1e75
Adam Bigstar <abhijeet@techmorphosis.com>
2016-08-09 14:54:20 +0530

    first commit

---
+++ app/src/main/java/com/jwplayer/opensourcedemo/JWPlayerViewExample.java
@@ -0,0 +1,176 @@
+package com.jwplayer.opensourcedemo;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.longtailvideo.jwplayer.JWPlayerView;
+import com.longtailvideo.jwplayer.cast.CastManager;
+import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents;
+import com.longtailvideo.jwplayer.media.ads.Ad;
+import com.longtailvideo.jwplayer.media.ads.AdBreak;
+import com.longtailvideo.jwplayer.media.ads.AdSource;
+import com.longtailvideo.jwplayer.media.playlists.PlaylistItem;
+
+import java.util.LinkedList;
+
+public class JWPlayerViewExample extends AppCompatActivity implements VideoPlayerEvents.OnFullscreenListener {
+
+	/**
+	 * Reference to the {@link JWPlayerView}
+	 */
+	private JWPlayerView mPlayerView;
+
+	/**
+	 * An instance of our event handling class
+	 */
+	private JWEventHandler mEventHandler;
+
+	/**
+	 * Reference to the {@link CastManager}
+	 */
+	private CastManager mCastManager;
+
+	/**
+	 * Stored instance of CoordinatorLayout
+	 * http://developer.android.com/reference/android/support/design/widget/CoordinatorLayout.html
+	 */
+	private CoordinatorLayout mCoordinatorLayout;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_jwplayerview);
+		mPlayerView = (JWPlayerView)findViewById(R.id.jwplayer);
+		TextView outputTextView = (TextView)findViewById(R.id.output);
+
+		mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.activity_jwplayerview);
+
+		// Handle hiding/showing of ActionBar
+		mPlayerView.addOnFullscreenListener(this);
+
+		// Keep the screen on during playback
+		new KeepScreenOnHandler(mPlayerView, getWindow());
+
+		// Instantiate the JW Player event handler class
+		mEventHandler = new JWEventHandler(mPlayerView, outputTextView);
+
+		// Construct a new Ad
+        //String tag = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/316192035/preroll&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&correlator=1234567890";
+        String tag = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=";
+        //String tag = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/316192035/bigstar-dev-mobile&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&correlator=";
+
+        Ad ad = new Ad(AdSource.IMA, tag);
+
+		// Construct a new AdBreak containing the Ad
+		AdBreak adPre = new AdBreak("pre", ad);
+		AdBreak adBreak = new AdBreak("00:00:10:000", ad);
+
+		// Create a new AdSchedule containing the AdBreak we just created
+		LinkedList<AdBreak> schedule = new LinkedList<>();
+		schedule.add(adPre);
+		schedule.add(adBreak);
+
+		// Load a media source
+		PlaylistItem pi = new PlaylistItem.Builder()
+				.file("https://content.jwplatform.com/videos/hWF9vG66-TNpruJId.mp4")
+				.adSchedule(schedule)
+				.build();
+
+		mPlayerView.load(pi);
+
+        mPlayerView.play();
+
+		// Get a reference to the CastManager
+		mCastManager = CastManager.getInstance();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		// Set fullscreen when the device is rotated to landscape
+		mPlayerView.setFullscreen(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE, true);
+		super.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	protected void onResume() {
+		// Let JW Player know that the app has returned from the background
+		mPlayerView.onResume();
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// Let JW Player know that the app is going to the background
+		mPlayerView.onPause();
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// Let JW Player know that the app is being destroyed
+		mPlayerView.onDestroy();
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// Exit fullscreen when the user pressed the Back button
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			if (mPlayerView.getFullscreen()) {
+				mPlayerView.setFullscreen(false, true);
+				return false;
+			}
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	/**
+	 * Handles JW Player going to and returning from fullscreen by hiding the ActionBar
+	 *
+	 * @param fullscreen true if the player is fullscreen
+	 */
+	@Override
+	public void onFullscreen(boolean fullscreen) {
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			if (fullscreen) {
+				actionBar.hide();
+			} else {
+				actionBar.show();
+			}
+		}
+
+		// When going to Fullscreen we want to set fitsSystemWindows="false"
+		mCoordinatorLayout.setFitsSystemWindows(!fullscreen);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_jwplayerview, menu);
+		// Register the MediaRouterButton on the JW Player SDK
+		mCastManager.addMediaRouterButton(menu, R.id.media_route_menu_item);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.switch_to_fragment:
+				Intent i = new Intent(this, JWPlayerFragmentExample.class);
+				startActivity(i);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+}
